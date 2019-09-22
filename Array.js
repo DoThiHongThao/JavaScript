7b1da72ecc38ca5ec4f47bf3415edd6ba3dc7a64
= <=>
2019-09-22 16:50:34 +0700

    commit to repository

---
+++ Array.js
@@ -0,0 +1,27 @@
+/*var Dog1={
+    name: 'Ngáo',
+    type: 'Husky'
+}
+var Dog2={
+    name: 'Lợn',
+    type: 'Chó tây'
+}
+var Dog3={
+    name: 'Inu',
+    type: 'Shiba'
+}
+
+var Dogs = [Dog1, Dog2, Dog3];
+console.log(Dogs[2].name);*/
+// Tạo danh sách lớp bằng array
+
+var hocSinh=[
+    {name:'Đỗ Thị Hồng Thảo', phone:'0294884384'},
+    {name:'Nguyễn Thị Thu Phương', phone:'0294893793'},
+    {name:'Hoàng Minh Nguyệt', phone:'0928493748'},
+    {name:'Hoàng Thị Thu Trang', phone:'0284738733'},
+    {name:'Hoàng Trung Hiếu', phone:'0299473847'},
+    {name:'Phạm Thùy Chi', phone:'0249375835'},
+]
+
+console.log(hocSinh);
